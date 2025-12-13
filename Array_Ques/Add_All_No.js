fc8706c99c2424f66fb7cdff9e508a653255c6b3
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-12-13 09:44:36 +0530

    Add-all

--- Array_Ques/Add_All_No.js
+++ Array_Ques/Add_All_No.js
@@ -12,6 +12,12 @@ function Sum(){
 console.log(Sum());
 
 
-// i = 0  arr[i] = 1 sum = 0+1 = 1
-// i = 1  arr[i] = 2 sum = 1+2 = 3
+// i = 0   arr[i] = 1   sum = 0 + 1 = 1
+// i = 1   arr[i] = 2   sum = 1 + 2 = 3
+// i = 2   arr[i] = 3   sum = 3 + 3 = 6
+// i = 3   arr[i] = 4   sum = 6 + 4 = 10
+// i = 4   arr[i] = 5   sum = 10 + 5 = 15
+
+// output
+//  15
 
