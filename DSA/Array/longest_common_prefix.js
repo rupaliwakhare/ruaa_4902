f46097901ad50d57c731e86f01af95da0cb9a093
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-03-09 19:55:35 +0530

    longest-common-prefix

---
+++ DSA/Array/longest_common_prefix.js
@@ -0,0 +1,18 @@
+const strs = ["flower", "flow", "flight"];
+
+const longestCommonPrefix  = (strs)=>{
+    if(strs.length === 0) return "";
+
+    let prefix = strs[0]
+
+    for(let i = 1; i<strs.length; i++){
+        while(strs[i].indexOf(prefix)!==0){
+            prefix=prefix.slice(0,prefix.length-1);
+            if(prefix === "") return "";
+
+        }
+    }
+    return prefix
+}
+
+console.log(longestCommonPrefix(strs));
